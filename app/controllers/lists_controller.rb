897e05fd797ab3946a7fec1bd65cb9f5ef06a0ba
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-07 06:42:30 +0000

    [finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,21 @@
 class ListsController < ApplicationController
-  def new
-  end
 
-  def index
-  end
+ def new
+    @list = List.new
+ end
 
-  def show
-  end
+ def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+ end
 
-  def edit
-  end
+ private
+  # ストロングパラメータ
+ def list_params
+ params.require(:list).permit(:title, :body)
+ end
 end
